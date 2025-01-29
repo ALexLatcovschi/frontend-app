af300590c297137ad4ab18cef9bbb9a28f9afecd
alexandru <alexandru@gmail.com>
2025-01-29 12:40:12 +0200

    add lesson 3 -> 1 section

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -17,6 +17,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     await loadComponent('main', '../../components/main.html');
 
     await loadComponent('header', '../../components/header.html');
+    await loadComponent('top-screen', '../../components/top-screen.html');
     await loadComponent('footer', '../../components/footer.html');
     document.getElementById('year').textContent = new Date().getFullYear();
 
