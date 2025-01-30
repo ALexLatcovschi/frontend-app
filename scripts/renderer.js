b0f5ef10b8a1bdbf2c1e253da6581027e210d85d
alexandru <alexandru@gmail.com>
2025-01-30 20:02:33 +0200

    add map

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -28,5 +28,8 @@ document.addEventListener('DOMContentLoaded', async () => {
     await loadComponent('reviews', '../../components/reviews.html');
     loadReviews();
 
+    await loadComponent('map', '../../components/map.html');
+    loadMap();
+
     await initTranslation();
 })
