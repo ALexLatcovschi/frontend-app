b820c82de7b9e4585f0caa5ca2f7c32e297b3a4d
alexandru <alexandru@gmail.com>
2025-02-01 13:45:22 +0200

    add hamburger menu

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -26,36 +26,57 @@ function hideLoader() {
     }
 }
 
+function toggleHamburgerMenu() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const button = document.getElementById('hamburger-button');
+
+    if (mobileMenu) {
+        if (mobileMenu.classList.contains('active')) {
+            mobileMenu.classList.remove('active')
+        } else {
+            mobileMenu.classList.add('active')
+        }
+    }
+
+    if (button) {
+        if (button.classList.contains('active')) {
+            button.classList.remove('active')
+        } else {
+            button.classList.add('active')
+        }
+    }
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
     await loadComponent('main', '../../components/main.html');
 
     await loadComponent('header', '../../components/header.html');
-    await loadComponent('top-screen', '../../components/top-screen.html');
+    // await loadComponent('top-screen', '../../components/top-screen.html');
 
-    await loadComponent('footer', '../../components/footer.html');
-    document.getElementById('year').textContent = new Date().getFullYear();
+    // await loadComponent('footer', '../../components/footer.html');
+    // document.getElementById('year').textContent = new Date().getFullYear();
 
-    await loadComponent('characteristics', '../../components/characteristics.html');
-    await loadComponent('expertise', '../../components/expertise.html');
+    // await loadComponent('characteristics', '../../components/characteristics.html');
+    // await loadComponent('expertise', '../../components/expertise.html');
 
-    await loadComponent('reviews', '../../components/reviews.html');
-    loadReviews();
+    // await loadComponent('reviews', '../../components/reviews.html');
+    // loadReviews();
 
-    await loadComponent('banner', '../../components/banner.html');
-    await loadComponent('faq', '../../components/faq.html');
-    loadFAQ();
+    // await loadComponent('banner', '../../components/banner.html');
+    // await loadComponent('faq', '../../components/faq.html');
+    // loadFAQ();
 
-    await loadComponent('contacts', '../../components/contacts.html');
-    await loadComponent('contact-form', '../../components/form.html');
-    initForm();
+    // await loadComponent('contacts', '../../components/contacts.html');
+    // await loadComponent('contact-form', '../../components/form.html');
+    // initForm();
 
-    await loadComponent('map', '../../components/map.html');
+    // await loadComponent('map', '../../components/map.html');
 
     await initTranslation();
 
     setTimeout(() => {
         hideLoader();
 
-        loadMap();
+        //     loadMap();
     }, 1000);
 })
